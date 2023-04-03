53488623e49811cb54da1e3446e0e91370545b68
hunglp9397 <lephihung0997@gmail.com>
2023-04-04 00:21:53 +0700

    create topic done

--- Source/code/1-kafka-cli/0-kafka-topics.sh
+++ Source/code/1-kafka-cli/0-kafka-topics.sh
@@ -1,5 +1,5 @@
 # Replace "kafka-topics.sh" 
-# by "kafka-topics" or "kafka-topics.bat" based on your system # (or bin/kafka-topics.sh or bin\windows\kafka-topics.bat if you didn't setup PATH / Environment variables)
+# by "kafka-topics" or "kafka-topics.bat" based on your system # (or bin/kafka-topics or bin\windows\kafka-topics.bat if you didn't setup PATH / Environment variables)
 
 
 ############################
@@ -13,25 +13,26 @@ sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule require
 sasl.mechanism=PLAIN
 
 # Start running commands
-kafka-topics.sh 
+kafka-topics 
 
-kafka-topics.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092
+kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092
 
-kafka-topics.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --create --topic first_topic
+kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --create --topic first_topic --partitions 3 --replication-factor 1
 
-kafka-topics.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --create --topic second_topic --partitions 5
+
+kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --create --topic second_topic --partitions 5
 
 # this will create a topic with an RF of 3 (behaviour of using Conduktor Platform - all topics have the same RF of 3)
-kafka-topics.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --create --topic third_topic --replication-factor 2
+kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --create --topic third_topic --replication-factor 2
 
 # list topics
 kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --list
 
 # describe topics
-kafka-topics.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --topic first_topic --describe
+kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --topic first_topic --describe
 
 # delete topics
-kafka-topics.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --topic second_topic --delete
+kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --topic second_topic --delete
 
 
 
@@ -39,25 +40,25 @@ kafka-topics.sh --command-config playground.config --bootstrap-server cluster.pl
 #####     LOCALHOST    #####
 ############################
 
-kafka-topics.sh 
+kafka-topics 
 
-kafka-topics.sh --bootstrap-server localhost:9092 --list 
+kafka-topics --bootstrap-server localhost:9092 --list 
 
-kafka-topics.sh --bootstrap-server localhost:9092 --topic first_topic --create
+kafka-topics --bootstrap-server localhost:9092 --topic first_topic --create
 
-kafka-topics.sh --bootstrap-server localhost:9092 --topic second_topic --create --partitions 3
+kafka-topics --bootstrap-server localhost:9092 --topic second_topic --create --partitions 3
 
-kafka-topics.sh --bootstrap-server localhost:9092 --topic third_topic --create --partitions 3 --replication-factor 2
+kafka-topics --bootstrap-server localhost:9092 --topic third_topic --create --partitions 3 --replication-factor 2
 
 # Create a topic (working)
-kafka-topics.sh --bootstrap-server localhost:9092 --topic third_topic --create --partitions 3 --replication-factor 1
+kafka-topics --bootstrap-server localhost:9092 --topic third_topic --create --partitions 3 --replication-factor 1
 
 # List topics
-kafka-topics.sh --bootstrap-server localhost:9092 --list 
+kafka-topics --bootstrap-server localhost:9092 --list 
 
 # Describe a topic
-kafka-topics.sh --bootstrap-server localhost:9092 --topic first_topic --describe
+kafka-topics --bootstrap-server localhost:9092 --topic first_topic --describe
 
 # Delete a topic 
-kafka-topics.sh --bootstrap-server localhost:9092 --topic first_topic --delete
+kafka-topics --bootstrap-server localhost:9092 --topic first_topic --delete
 # (only works if delete.topic.enable=true)
--- kafka_cli/README.md
+++ kafka_cli/README.md
@@ -1,3 +1,26 @@
 **KAFKA CLI**
 
 [Source : ](../Source/code/1-kafka-cli)
+
+**Folder thực hành project** :  _/kafka_series/kafka_cli_
+
+**B1:** Vô trang conduktor.io tạo tài khoản
+
+Sau đó bấm vô Playground
+
+Thu được kết quả như hình : 
+![img.png](img.png)
+
+**B2:** Cd tới thư mục kafka_cli
+
+ Tạo file playground.config với content là cái vừa tạo ở B1
+![img_1.png](img_1.png)
+
+
+B3: Vẫn trong thư mục kafka_cli, tạo topics bằng lệnh : `kafka-topics --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --create --topic first_topic --partitions 3 --replication-factor 1`
+
+=> 
+Kết quả : 
+![img_2.png](img_2.png)
+
+![img_3.png](img_3.png)
