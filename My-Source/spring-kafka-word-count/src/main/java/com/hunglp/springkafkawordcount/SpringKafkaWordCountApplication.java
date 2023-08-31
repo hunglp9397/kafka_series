675ae0a43c075f75659635496be867bd984af245
hunglp9397 <lephihung0997@gmail.com>
2023-09-01 02:43:15 +0700

    init spring kafka stream project

---
+++ My-Source/spring-kafka-word-count/src/main/java/com/hunglp/springkafkawordcount/SpringKafkaWordCountApplication.java
@@ -0,0 +1,13 @@
+package com.hunglp.springkafkawordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaWordCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaWordCountApplication.class, args);
+	}
+
+}
